46e47b473c9ef0370e9f61f8a170793b0482cc7e
TsertsvadzeE <eliso.tsertsvadze99@gmail.com>
2025-04-24 11:40:58 +0400

    Footer component

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+      <footer className="bg-gray-100 text-center text-sm text-gray-500 py-6 mt-10 border-t">
+        <div className="container mx-auto">
+          <p>© {new Date().getFullYear()} შავი ოქრო.</p> 
+          <p> ყველა უფლება დაცულია.</p>
+          <p>დაგვიკავშირდით: contact@shaviokro.ge</p>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
